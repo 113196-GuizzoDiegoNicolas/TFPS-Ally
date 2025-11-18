d797098a09a3c1ab77821e3fe9b0c9d43ee56f80
INDIRADSM <89947499+INDIRADSM@users.noreply.github.com>
2025-11-18 20:12:57 -0300

    c2
    modificaciones

--- Ally/src/main/resources/data.sql
+++ Ally/src/main/resources/data.sql
@@ -83,10 +83,15 @@ INSERT INTO transportistas (
 -- =====================================
 -- SERVICIOS (opcional)
 -- =====================================
-INSERT INTO services (paciente_id, prestador_id, especialidad, descripcion, estado, fecha_solicitud)
+INSERT INTO service_requests
+(paciente_id, prestador_id, especialidad, descripcion, estado, activo, fecha_solicitud)
 VALUES
-    (1, 1, 'KINESIOLOGIA', 'Rehabilitación post operatoria rodilla derecha', 'PENDIENTE', CURRENT_TIMESTAMP),
-    (2, 2, 'TERAPIA_OCUPACIONAL', 'Terapia para mejora de habilidades motoras finas', 'CONFIRMADO', CURRENT_TIMESTAMP);
+    (1, 1, 'KINESIOLOGIA', 'Rehabilitación post operatoria rodilla derecha', 'PENDIENTE', true, CURRENT_TIMESTAMP);
+
+INSERT INTO service_requests
+(paciente_id, prestador_id, especialidad, descripcion, estado, activo, fecha_solicitud)
+VALUES
+    (2, 2, 'TERAPIA_OCUPACIONAL', 'Terapia para mejora de habilidades motoras finas', 'CONFIRMADO', true, CURRENT_TIMESTAMP);
 
 -- =====================================
 -- COMENTADO: SERVICIOS DE TRANSPORTE
