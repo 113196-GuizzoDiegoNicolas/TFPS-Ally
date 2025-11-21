c65149ca9501084e3f2e03d99e5cb7af9e90ff00
INDIRADSM <89947499+INDIRADSM@users.noreply.github.com>
2025-11-21 19:37:52 -0300

    modifi

--- Ally/src/main/java/Ally/Scafolding/entities/ProvidersEntity.java
+++ Ally/src/main/java/Ally/Scafolding/entities/ProvidersEntity.java
@@ -1,27 +1,44 @@
 package Ally.Scafolding.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.envers.Audited;
 import org.hibernate.envers.RelationTargetAuditMode;
 
+/**
+ * Represents a medical provider in the database.
+ * <p>
+ * This entity stores medical provider information including specialty and availability.
+ * </p>
+ */
 @Entity
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "providers")
 @Audited
 public class ProvidersEntity extends PersonsEntity {
 
-    @ManyToOne
-    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    /**
+     * Identificador único del prestador.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Specialty code of the medical provider.
+     */
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "codigo_especialidad")
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
     private SpecialtyEntity especialidad;
 
+    /**
+     * Indicates if the provider is currently active.
+     */
     @Column(name = "activo", nullable = false)
     private Boolean activo = true;
 }
-
-
