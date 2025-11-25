3642f1aa13d35c426ea9dad678f9310307666d7b
113196_GUIZZO <113196@tecnicatura.frc.utn.edu.ar>
2025-11-25 19:51:26 -0300

    commit loginRespndeDTO

--- Ally/src/main/java/Ally/Scafolding/dtos/common/login/LoginResponseDTO.java
+++ Ally/src/main/java/Ally/Scafolding/dtos/common/login/LoginResponseDTO.java
@@ -21,7 +21,7 @@ public class LoginResponseDTO {
     /**
      * User information.
      */
-    private UserDTO user;
+    private UserDTO usuario;
 
     /**
      * Role of the authenticated user.
