1c6a19dc44396d836144bc068d16afad40c8a109
INDIRADSM <89947499+INDIRADSM@users.noreply.github.com>
2025-12-04 22:52:27 -0300

    modi

--- Ally/src/main/java/Ally/Scafolding/dtos/common/admin/AdminMetricsDTO.java
+++ Ally/src/main/java/Ally/Scafolding/dtos/common/admin/AdminMetricsDTO.java
@@ -1,18 +1,15 @@
 package Ally.Scafolding.dtos.common.admin;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdminMetricsDTO {
     private long pacientes;
     private long prestadores;
     private long transportistas;
     private long admins;
-
-    public AdminMetricsDTO(long pacientes, long prestadores, long transportistas, long admins) {
-        this.pacientes = pacientes;
-        this.prestadores = prestadores;
-        this.transportistas = transportistas;
-        this.admins = admins;
-    }
-
-    // Getters & Setters
-
 }
--- Ally/src/main/java/Ally/Scafolding/dtos/common/admin/AdminUserDTO.java
+++ Ally/src/main/java/Ally/Scafolding/dtos/common/admin/AdminUserDTO.java
@@ -1,17 +1,15 @@
 package Ally.Scafolding.dtos.common.admin;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdminUserDTO {
     private Long id;
-    private String nombre;
+    private String nombre; // O cambiar luego por usuario si hace falta
     private String rol;
     private boolean activo;
-
-    public AdminUserDTO(Long id, String nombre, String rol, boolean activo) {
-        this.id = id;
-        this.nombre = nombre;
-        this.rol = rol;
-        this.activo = activo;
-    }
-
-    // Getters & Setters
 }
