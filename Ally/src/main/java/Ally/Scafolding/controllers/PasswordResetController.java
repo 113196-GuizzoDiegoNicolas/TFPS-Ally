ba9a8675362ce62f27c550047307f7e7ff1ed2d6
INDIRADSM <89947499+INDIRADSM@users.noreply.github.com>
2026-02-13 01:55:47 -0300

    modificaciones

--- Ally/src/main/java/Ally/Scafolding/controllers/PasswordResetController.java
+++ Ally/src/main/java/Ally/Scafolding/controllers/PasswordResetController.java
@@ -3,9 +3,10 @@ package Ally.Scafolding.controllers;
 import Ally.Scafolding.dtos.common.login.ForgotPasswordRequest;
 import Ally.Scafolding.dtos.common.login.ResetPasswordRequest;
 import Ally.Scafolding.services.impl.PasswordResetService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,11 +21,15 @@ public class PasswordResetController {
 
     @PostMapping("/forgot-password")
     public ResponseEntity<Void> forgotPassword(@RequestBody ForgotPasswordRequest req) {
-        passwordResetService.forgotPassword(req.getEmail());
+        if (req.getEmail() == null || req.getEmail().isBlank()) {
+            return ResponseEntity.badRequest().build();
+        }
+        service.forgotPassword(req.getEmail().trim());
         return ResponseEntity.ok().build();
     }
+
     @PostMapping("/reset-password")
-    public ResponseEntity<?> resetPassword(@RequestBody ResetPasswordRequest req) {
+    public ResponseEntity<Void> resetPassword(@RequestBody ResetPasswordRequest req) {
         service.resetPassword(req.token(), req.newPassword());
         return ResponseEntity.ok().build();
     }
--- Ally/src/main/java/Ally/Scafolding/dtos/common/login/ForgotPasswordRequest.java
+++ Ally/src/main/java/Ally/Scafolding/dtos/common/login/ForgotPasswordRequest.java
@@ -7,4 +7,5 @@ import lombok.Setter;
 @Setter
 public class ForgotPasswordRequest {
     private String email;
+    public String getEmail() { return email; }
 }
